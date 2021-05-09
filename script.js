226d27f27a7f30247f05c9c02266dde33f06de15
Noor Jehan Rajper <rajpernoor@gmail.com>
2021-05-10 01:15:39 +0500

    contactForm files uploaded

---
+++ script.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector('button')
+const inputs = document.querySelector('form')
+btn.addEventListener('click', () => {
+
+    const name = (inputs.elements["name"].value).trim()
+    const email = (inputs.elements["email"].value).trim()
+    const msg = (inputs.elements["message"].value.trim())
+    const phone = (inputs.elements["phone"].value).trim()
+    if (!name.length > 0 || !email.length > 0 || !msg.length > 0 || !phone.length > 0) {
+        alert("All fields are mandatory")
+        return
+    }
+    Email.send({
+        // SecureToken: "5307ded4-d6de-496c-b0d4-df535e025caa",
+        Host: ".............",
+        Username: "...............",
+        Password: "..............",
+        To: "...........",
+        From: email,
+        Subject: "Contact Us Query By the Customer",
+        Body: msg + "<br>" + name + "<br>" + phone
+    }).then(msg => alert("The email successfully sent"))
+})
